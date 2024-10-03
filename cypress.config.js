69feed6f1e5caa8a4c9dd70b0f4b691bde2d7546
André Scheffer <andretcrs@gmail.com>
2024-10-03 10:41:59 -0300

    Curso-Cypress-Avançado

--- cypress.config.js
+++ cypress.config.js
@@ -2,11 +2,11 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
-    setupNodeEvents(on, config) {
+    screenshotsFolder: './cypress/screenshots',
+    setupNodeEvents (on, config) {
       return require('./cypress/plugins/index.js')(on, config)
     },
-    baseUrl: 'https://wlsf82-hacker-stories.web.app',
-  },
+    baseUrl: 'https://wlsf82-hacker-stories.web.app'
+
+  }
 })
